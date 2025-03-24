98e70fcf837c448558199587b792836e823a13c2
Henrysoed <henry.soedibjo@gmail.com>
2025-03-24 17:36:16 +0700

    initial commit

---
+++ player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed: float = 85
+@onready var animations = $AnimationPlayer
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection*speed
+	
+func _physics_process(delta: float) -> void:
+	handleInput()
+	move_and_slide()
+	updateAnimation()
+	
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.play("idle")
+	else:
+		var direction = "Down"
+		if velocity.x < 0:
+			direction = "Left"
+		elif velocity.x > 0:
+			direction = "Right"
+		elif velocity.y < 0:
+			direction = "Up"
+		animations.play("walk"+direction)
